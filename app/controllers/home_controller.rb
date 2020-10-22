c1518d243db9eb975ce3d16c182676e55714e13e
Muhammad Talha Waqar <talhawaqar007@gmail.com>
2020-10-22 12:24:46 +0500

    complete index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    featured_id =Vote.all.group(:article_id).count.map { |x,y| {id: x, count: y} }.max_by{|k,v| v}[:id]
+    @feature_article = Article.find(featured_id)
+    @categories = Category.all
   end
 end
